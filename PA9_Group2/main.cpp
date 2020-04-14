b672968b7010ae0544ec6bab33cdc3dfdad7de7a
KNatsuhara <kojiman@live.com>
2020-04-14 16:10:37 -0700

    print statement

--- PA9_Group2/main.cpp
+++ PA9_Group2/main.cpp
@@ -3,12 +3,16 @@ Authors: Derek Henderson, Koji Nastuhara, Nathan Brown, Sammy West
 Description: Epic tower defense game
 */
 
+#include <iostream>
+
 
 
 int main()
 {
 	//asdflkjlaksd
 	//leCode
+	std::cout << "Tower Defense!" << std::endl;
+
 
 	return 0;
 }
