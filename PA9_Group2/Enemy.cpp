9a077a60151891c7610b0bc91c9f4b74ce71209c
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-04-20 18:28:01 -0700

    Added more code to game: Load rounds, spawn enemies, move enemies
    Created Round class

--- PA9_Group2/Enemy.cpp
+++ PA9_Group2/Enemy.cpp
@@ -1,8 +1,29 @@
 
 #include "Enemy.h"
 
+Enemy::Enemy(void)
+{
+    isEnemy = true;
+}
 
-void Enemy::draw(sf::RenderWindow* window)
+void Enemy::move(Board& board)
+{
+    //Calculate distance to move from speed and potential frost/glue effect
+
+    switch (board.get_direction())
+    {
+    case UP:
+        break;
+    case DOWN:
+        break;
+    case LEFT:
+        break;
+    case RIGHT:
+        break;
+    }
+}
+
+void Enemy::draw(sf::RenderWindow& window)
 {
 
 }
