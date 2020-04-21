ead2c03e9ab8543a02eae88d0c35fabea9469d35
Derek Henderson <62639977+D3r3k23@users.noreply.github.com>
2020-04-20 22:25:40 -0700

    Small change

--- PA9_Group2/Game.cpp
+++ PA9_Group2/Game.cpp
@@ -43,7 +43,7 @@ void Game::run(void) //Refactor
             collision_handler();
         }
 
-        if (rounds[currentRound - 1].get_enemies_remaining() == 0 && numEnemies == 0)
+        if (rounds[currentRound - 1].spawning_complete() && numEnemies == 0)
         {
             currentRound++;
             roundStarted = false;
