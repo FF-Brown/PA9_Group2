fee29d160122f16297559164a740ab6da62399b3
KNatsuhara <55007517+KNatsuhara@users.noreply.github.com>
2020-04-28 13:41:47 -0700

    Update Tower.h

--- PA9_Group2/Tower.h
+++ PA9_Group2/Tower.h
@@ -1,2 +1,34 @@
 #pragma once
 //Base class for towers - range, dmg, price, price to upgrade(?)
+#pragma once
+
+#include "header.h"
+
+struct Bullet {
+    float radius;
+    sf::CircleShape shape;
+    sf::Vector2f currVelocity;
+    float maxSpeed;
+};
+
+class Tower {
+public:
+    sf::CircleShape shape;
+    sf::Vector2f playerCenter;
+    float range;
+    int atkSpeed;
+    int shootTimer;
+    int damage;
+    Bullet bullet;
+    std::vector<Bullet> bullets;
+
+    Tower()
+    {
+        this->shape.setRadius(30.f);
+        this->shape.setFillColor(sf::Color::White);
+        this->range = 1.2;
+        this->atkSpeed = 30; //The higher it is the slower they shoot
+        this->shootTimer = 0;
+        this->damage = 10;
+    }
+};
