d84d35fcf8da18b90e65a15484d3ae8b60c4db04
KNatsuhara <55007517+KNatsuhara@users.noreply.github.com>
2020-04-28 14:01:24 -0700

    Update Enemy.h

--- PA9_Group2/Enemy.h
+++ PA9_Group2/Enemy.h
@@ -1,3 +1,26 @@
 #pragma once
 
+#include "header.h"
+
+class Enemy {
+public:
+    sf::RectangleShape shape;
+    sf::Vector2f enemyPosWindow;
+    int health;
+    bool alive;
+
+    Enemy()
+    {
+        this->shape.setSize(sf::Vector2f(50.f, 50.f));
+        this->shape.setFillColor(sf::Color(255, 255, 0));
+        this->health = 255;
+        this->alive = true;
+    }
+
+    void checkHealth()
+    {
+        this->shape.setFillColor(sf::Color(255, 0 + health, 0));
+    }
+};
+
 //base class for enemies - HP, walking speed(??)
