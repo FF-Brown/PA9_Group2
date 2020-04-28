489c783767710382fb435b35af355c0ea3909e65
KNatsuhara <55007517+KNatsuhara@users.noreply.github.com>
2020-04-28 13:44:15 -0700

    Update Player.h

--- PA9_Group2/Player.h
+++ PA9_Group2/Player.h
@@ -1,3 +1,26 @@
 #pragma once
 
 //class for player - # of towers, amount of gold, total HP, etc
+
+#include "header.h"
+
+class Enemy {
+public:
+    sf::RectangleShape shape;
+    sf::Vector2f enemyPosWindow;
+    int health;
+    bool alive;
+
+    Enemy()
+    {
+        this->shape.setSize(sf::Vector2f(50.f, 50.f));
+        this->shape.setFillColor(sf::Color(255, 255, 0));
+        this->health = 255;
+        this->alive = true;
+    }
+
+    void checkHealth()
+    {
+        this->shape.setFillColor(sf::Color(255, 0 + health, 0));
+    }
+};
