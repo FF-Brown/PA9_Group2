cae0d3a7ce0e1ccfed66b0286add48a4cff6d624
sammy0west@gmail.com <sammy0west@gmail.com>
2020-04-28 19:19:40 -0700

    fixed instruction bug... probably

--- PA9_Group2/main_menu.cpp
+++ PA9_Group2/main_menu.cpp
@@ -166,19 +166,12 @@ bool Menu::display_instructions(sf::RenderWindow& window)
 {
     window.clear();
     sf::Text text[6];
-    sf::Font font;
-    Button button;
-    if (!font.loadFromFile("Resources/Chunk Five.otf"))	//font~~~
-    {
-        std::cout << "Error Loading Font" << std::endl;
-        return true;
-    }
 
     text[0].setString("How to play the Epic Tower Defence Game:");
     text[1].setString("Prevent Monsters from reaching your castle by placing");
-    text[2].setString(" towers along the path by clicking the button of the ");
+    text[2].setString("towers along the path by clicking the button of the ");
     text[3].setString("tower you want and then clicking the space where you");
-    text[4].setString(" want it placed");
+    text[4].setString("want it placed");
 
     for (int i = 0, h = 10; i < 5; i++)
     {
@@ -192,46 +185,73 @@ bool Menu::display_instructions(sf::RenderWindow& window)
         window.draw(text[i]);
     }
 
-    button.set_size(50, 80);
-    button.set_text("BACK", 25);
-    button.set_position(70, 450);
+	sf::RectangleShape button;
+	sf::Text back;
+	button.setSize(sf::Vector2f(100, 100));
+	button.setPosition(70, 450);
+	button.setFillColor(sf::Color::Green);
+
+    back.setString("BACK");
+	back.setCharacterSize(25);
+	back.setFont(font);
+	back.setFillColor(sf::Color::White);
+	back.setOutlineColor(sf::Color::Black);
+	back.setPosition(sf::Vector2f(70, 450));
+	back.setOutlineThickness(2);
+ 
+
+	int buttonX = 70;
+	int buttonY = 450;
+	int buttonW = 50;
+	int buttonH = 80;
 
 
-
-    sf::Event event;
-
-    bool exit = false;
-
-
+	sf::Event event;
+	bool exit = false;
     while (window.pollEvent(event))
     {
+		int mouseX = sf::Mouse::getPosition(window).x;
+		int mouseY = sf::Mouse::getPosition(window).y;
+
+		int buttonPosW = buttonX + buttonW;
+		int buttonPosH = buttonY + buttonH;
+
         switch (event.type)
         {
         case sf::Event::Closed:
             window.close();
             break;
         case sf::Event::MouseMoved:
-            for (int i = 0; i < 3; i++)
-            {
-                menu_buttons[i].is_over(window);
-            }
-
+            
+		if (mouseX < buttonPosW && mouseX > buttonX&& mouseY < buttonPosH && mouseY > buttonY)
+		{
+			button.setOutlineThickness(2);
+			button.setOutlineColor(sf::Color::Red);
+		}
+		else {
+			button.setOutlineThickness(2);
+			button.setOutlineColor(sf::Color::Green);
+		}
             break;
         case sf::Event::MouseButtonPressed:
-            for (int i = 0; i < 3; i++)
-            {
-                if (menu_buttons[i].is_over(window))
-                {
-                    exit = true;
-                }
-            }
-
+             
+			if (mouseX < buttonPosW && mouseX > buttonX&& mouseY < buttonPosH && mouseY > buttonY)
+			{
+				button.setOutlineThickness(2);
+				button.setOutlineColor(sf::Color::Red);
+				exit = true;
+			}
+			else {
+				button.setOutlineThickness(2);
+				button.setOutlineColor(sf::Color::Green);
+			}
             break;
         }
     }
 
     window.draw(text[5]);
-    button.draw_display(window);
+	window.draw(button);
+	window.draw(back);
 
 
     return exit;
